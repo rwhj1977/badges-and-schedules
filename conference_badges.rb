8bddd7bcb4267552ec30c8e5ad07915d669417e0
Robert Hardy <rwhj77@hotmail.com>
2021-01-18 19:49:15 -0600

    commit tree 5a2b1a92e54006f5281cb3b6ab09b5174e92ba74

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+  end
+  
+  def batch_badge_creator(names)
+    list1 = []
+    names.each do |name|
+     list1.push("Hello, my name is #{name}.")
+    end
+    return list1
+  end
+  
+  def assign_rooms(names)
+    list2 = []
+    names.each_with_index do |name, index|
+      final = index+1
+      list2.push("Hello, #{name}! You'll be assigned to room #{final}!")
+    end
+    return list2
+  end
+  
+  def printer(names)
+    batch_badge_creator(names).each do |badge|
+      puts badge
+    end
+     assign_rooms(names).each do |assignment|
+        puts assignment
+    end
+end
